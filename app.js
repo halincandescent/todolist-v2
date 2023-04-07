2e804c18e2125e4a6bf54bb950d96879ad063e8a
Tate Casey <tate@Tates-MBP.hitronhub.home>
2023-04-07 16:38:59 -0400

    added lodash to deal with the various url typings of the express route parameters to stop duplication in db and keep lists together

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
- 
+const _ = require("lodash");  
  
 const app = express();
  
@@ -99,7 +99,7 @@ app.post("/delete", function(req, res){
 });
 
 app.get("/:customListName", function(req, res){
-  const customListName = req.params.customListName;
+  const customListName = _.capitalize(req.params.customListName);
   
   List.findOne({name: customListName}).then(function(foundList){
      
